68ebf24792078ab6bf24caf7a7f9d9a81e1588c7
JamesParrott <80779630+JamesParrott@users.noreply.github.com>
2024-11-17 22:29:24 +0000

    Add math operation buttons

--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,11 +1,23 @@
 import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
 import { Welcome } from '../components/Welcome/Welcome';
+import { Text, Button, Group } from '@mantine/core';
+
 
 export function HomePage() {
   return (
     <>
       <Welcome />
-      <ColorSchemeToggle />
+      <ColorSchemeToggle  />
+      <Text c="dimmed" ta="center" size="lg" maw={580} mx="auto" mt="xl">
+        Play the game below!!!
+      </Text>
+      
+    <Group justify="center" mt="xl">
+    <Button >+</Button>
+    <Button >-</Button>
+    <Button >*</Button>
+    <Button >÷</Button>
+    </Group>
     </>
   );
 }
