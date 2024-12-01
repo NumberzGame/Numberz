e7e22d3cbd2ca9dc5079cdc1d718865e87feea39
JamesParrott <80779630+JamesParrott@users.noreply.github.com>
2024-12-01 16:25:48 +0000

    Update NumbersGame.tsx

--- src/components/NumbersGame/NumbersGame.tsx
+++ src/components/NumbersGame/NumbersGame.tsx
@@ -13,18 +13,21 @@ const overrideSymbolText = function(s: string): string {
   return s
 }
 
-// https://mantine.dev/hooks/use-local-storage/
-// const [playHistory, setplayHistory] = useLocalStorage({ key: 'play-history', defaultValue: '' });
-
-// currentGameState currentGame
-// time_stamp_first_played_mins Date.now() / 1000 / 60
-// current game grade
-// current game goal
-// current game form
-// current game index (index all inc forms, and figure out using JSON dict of frequencies?)
-// const [currentGameState, setCurrentGameState] = useLocalStorage({ key: 'current-game-state', defaultValue: '' });
 
 export function NumbersGame() {
+
+    // https://mantine.dev/hooks/use-local-storage/
+    const [playHistory, setplayHistory] = useLocalStorage({ key: 'play-history', defaultValue: '' });
+
+    // currentGameState currentGame
+    // time_stamp_first_played_mins Date.now() / 1000 / 60
+    // current game grade u13 (1,..,223) under current Heuristic (additional difficulty)
+    // current game goal u15 (100,...,999)
+    // current game form u10 {2, 3, 4, (2, 2), 5, (3, 2), (1, (2, 2)), 6, (4, 2), (3, 3), 
+    // (2, (2, 2)), (1, (3, 2)), (1, (1, (2, 2)) },
+    // current game index u25, max is grade 18 with 781176 (20 bits) non de-duped solutions
+    // (index all inc forms, and figure out using JSON dict of frequencies?)
+    const [currentGameState, setCurrentGameState] = useLocalStorage({ key: 'current-game-state', defaultValue: '' });
 
     const SymbolsButtons = SYMBOLS.OPS.map((s: string) => (<Button >{overrideSymbolText(s)}</Button>));
 
