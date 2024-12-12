b23037eb0f6bf9e06521e94944be12580b3d75e0
JamesParrott <80779630+JamesParrott@users.noreply.github.com>
2024-12-12 14:22:05 +0000

    Add type hints solver, rename to solver_dfs, and move into ./src/components/NumbersGame.

--- src/components/NumbersGame/solver_dfs.ts
+++ src/components/NumbersGame/solver_dfs.ts
@@ -9,7 +9,7 @@ const INVALID_ARGS = Symbol();
 console.log(`Goal: ${goal}`);
 console.log(`Seeds: ${seeds}`);
 
-function* allPairsFrom(arr) {
+function* allPairsFrom<T>(arr: T[]): IterableIterator<[[number,T],[number,T]]> {
     for (const [i, first_item] of arr.slice(0,-1).entries()) {
         for (const [j,second_item] of arr.slice(i+1).entries()) {
             yield [[i,first_item], [i+1+j,second_item]];
@@ -21,34 +21,41 @@ function* allPairsFrom(arr) {
 
 
 const OPS = {
-    '+' : (x, y) => x+y,
-    '*' : (x, y) => x*y,
-    '-' : (x, y) => Math.abs(x-y),
-    '/' : (x, y) => x % y === 0 ? x / y : 
+    '+' : (x: number, y: number) => x+y,
+    '*' : (x: number, y: number) => x*y,
+    '-' : (x: number, y: number) => Math.abs(x-y),
+    '/' : (x: number, y: number) => x % y === 0 ? x / y : 
                       y % x === 0 ? y / x : 
                         INVALID_ARGS,
 };
 
 
 
+class Operand {
+    readonly val: number;
+    readonly expr: string;
+    
+    constructor(val: number, expr: string | null = null) {
+        this.val = val;
+        this.expr = expr ?? val.toString(10);
+    }
+}
 
-function makeSubSolExpr(x, y, op) {
+function makeSubSolExpr(x: Operand, y: Operand, op: string): string {
     return `(${x.expr} ${op} ${y.expr})`
 }
 
 
-function makeValidSubSolExpr(x, y, op) {
+function makeValidSubSolExpr(x: Operand, y: Operand, op: string): string {
     // Ensure x >= y for abs (x - y) and /
     return x.val >= y.val ? makeSubSolExpr(x, y, op) : makeSubSolExpr(y, x, op);
 }
 
 
-function makeOperandObject(val, expr = null) {             
-    return {"val": val, "expr": expr ?? val.toString(10)};
-}
 
 
-function* allOperandsFrom(operands, maxDepth= 6) {
+
+function* allOperandsFrom(operands: Operand[], maxDepth: number = 6): IterableIterator<Operand[]> {
     if (maxDepth ===0 || operands.length === 1) {
         return;
     }
@@ -66,7 +73,7 @@ function* allOperandsFrom(operands, maxDepth= 6) {
                 continue;
             }
             const subSolExpr = makeValidSubSolExpr(x, y, symbol);
-            const resultOperand = makeOperandObject(result, subSolExpr);
+            const resultOperand = new Operand(result, subSolExpr);
             const nextOperands = remainingOperands.concat(resultOperand);
             yield nextOperands;
             for (const moreOperands of allOperandsFrom(nextOperands, maxDepth-1)) {
@@ -78,18 +85,19 @@ function* allOperandsFrom(operands, maxDepth= 6) {
 }
 
 
-const operandsFromSeeds = function() {
-    return allOperandsFrom(seeds.map((seed) => makeOperandObject(seed, null)));
+const operandsFromSeeds = function(): IterableIterator<Operand[]>  {
+    return allOperandsFrom(seeds.map((seed) => new Operand(seed, null)));
 }
 
 
-
-for (const operands of operandsFromSeeds()) {
-    // console.log(`operands: ${Array.from(operands.map(op => op.expr))}`);
-    // continue;
-    for (const operand of operands) {
-        if (operand.val === goal) {
-            console.log(operand.expr);
+export const solutions = function*(goal: number): IterableIterator<Operand> {
+    for (const operands of operandsFromSeeds()) {
+        // console.log(`operands: ${Array.from(operands.map(op => op.expr))}`);
+        // continue;
+        for (const operand of operands) {
+            if (operand.val === goal) {
+                yield operand;
+            }
         }
     }
 }
