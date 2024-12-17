bde32cd759c0e5f986780e547a809ec8fe68d256
JamesParrott <80779630+JamesParrott@users.noreply.github.com>
2024-12-17 12:37:26 +0000

    Update GameBoSelector.tsx

--- src/components/NumbersGame/GameBoSelector.tsx
+++ src/components/NumbersGame/GameBoSelector.tsx
@@ -1,6 +1,6 @@
 
 
-import {Text } from '@mantine/core';
+import {Text, Stack } from '@mantine/core';
 
 
 import {
@@ -18,24 +18,39 @@ export function GameBoSelector() {
     queryKey: ['repoData'],
     queryFn: async () => {
       const response = await fetch(
-        'https://api.github.com/repos/TanStack/query',
+        './grades_goals_solutions_forms/22/distribution.json',
       )
       return await response.json()
     },
   })
 
-  if (isPending) return 'Loading...'
+  if (isPending) {
+    return 'Loading game...';
+  }
 
-  if (error) return 'An error has occurred: ' + error.message
+  if (error) {
+    return 'An error has occurred: ' + error.message;
+  }
+  
+  if (isFetching) {
+    return 'Fetching game... ';
+  }
+
+  const forms: string[] = Array.from(Object.keys(data));
+  const freqs: number[] = Array.from(Object.values(data));
+  const FormTexts = Object.entries(data).map(([k, v]: [string, any]) => (<Text>{k} : {v.toString()}</Text>));
 
   return (
-      <div>
-        <h1>{data.full_name}</h1>
-        <p>{data.description}</p>
-        <strong>👀 {data.subscribers_count}</strong>{' '}
-        <strong>✨ {data.stargazers_count}</strong>{' '}
-        <strong>🍴 {data.forks_count}</strong>
-        <div>{isFetching ? 'Updating...' : ''}</div>
-      </div>
+      // <div>
+      //   <h1>{data.full_name}</h1>
+      //   <p>{data.description}</p>
+      //   <strong>👀 {data.subscribers_count}</strong>{' '}
+      //   <strong>✨ {data.stargazers_count}</strong>{' '}
+      //   <strong>🍴 {data.forks_count}</strong>
+      //   <div>{isFetching ? 'Updating...' : ''}</div>
+      // </div>
+      <Stack>
+        {FormTexts}
+      </Stack>
   )
 }
