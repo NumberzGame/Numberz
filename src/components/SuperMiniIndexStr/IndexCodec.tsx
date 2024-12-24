e1b60267e70b46deb087012aad35b0334f07ec22
JamesParrott <80779630+JamesParrott@users.noreply.github.com>
2024-12-24 19:13:02 +0000

    Update IndexCodec.tsx

--- src/components/SuperMiniIndexStr/IndexCodec.tsx
+++ src/components/SuperMiniIndexStr/IndexCodec.tsx
@@ -1,11 +1,25 @@
-import { intDecoder } from 'sub_byte';
+import {FORMS} from '../NumbersGame/Core';
+import { intEncoder, intDecoder } from 'sub_byte';
    
 export const spacer = '\u8000';
 
 
+function next<T>(iterator: IterableIterator<T>): T {
+    const result = iterator.next();
+    if (result.done) {
+        throw new Error(`Decoding error. Ran out of code-units in string.`);
+    }
+    return result.value;
+}
+
+
 class FormsAndFreqs{
-    raw_bytes: Uint8Array;
-    
+    rawBytes: Uint8Array;
+    totalNumForms: number | null;
+    numFormsInThisSection: number | null;
+    numFormsToGo: number | null;
+    pastZeroMarker: boolean;
+
     constructor(str: string) {
         // const ords = [ord(c) for c in str]
         // It makes no difference as we're only encoding using the first 
@@ -18,55 +32,73 @@ class FormsAndFreqs{
         // "Warning: When the empty string ("") is used as a separator, 
         // the string is not split by user-perceived characters..."
         const ords = str.split("").map((c) => c.charCodeAt(0));
-        // this.raw_bytes = bytes(sub_byte.factories.int_encoder(ords, [15]))
-        this.raw_bytes = new Uint8Array(intDecoder(ords, null, [15]))
-        // this.total_num_forms = null
-        // this.num_forms_in_this_section = null
-        // this.num_forms_to_go = null
-        // this.past_zero_marker = false
-        // print(f"{this.raw_bytes=}")
+        // this.rawBytes = bytes(sub_byte.factories.int_encoder(ords, [15]))
+        this.rawBytes = new Uint8Array(intEncoder(ords, [15]));
+        this.totalNumForms = null
+        this.numFormsInThisSection = null
+        this.numFormsToGo = null
+        this.pastZeroMarker = false;
+        // print(f"{this.rawBytes=}")
     }
-    // def bit_widths(self):
-    //     # Total length
-    //     yield 4
 
-    //     while True:
-    //         if this.past_zero_marker:
-    //             yield 4
-    //             yield 12
-    //         else:
-    //             yield 4
 
-    // def __call__(self) -> Iterator[tuple[Literal[*list(ONE_INDEXED_FORMS)], int]]:
-    //     uint_iterator = sub_byte.factories.int_decoder(
-    //         iter(this.raw_bytes), None, this.bit_widths()
-    //     )
-    //     this.total_num_forms = this.num_forms_to_go = next(uint_iterator)
-    //     for uint in uint_iterator:
-    //         this.num_forms_in_this_section = next(uint_iterator)
-    //         if uint == 0b000:
-    //             assert this.num_forms_in_this_section == this.num_forms_to_go
-    //             this.past_zero_marker = True
-    //             break
+    *bitWidths(): IterableIterator<4 | 12>{
+        // Total length
+        yield 4;
 
-    //         shortcut_freq = uint
+        while (true) {
+            if (this.pastZeroMarker) {
+                yield 4;
+                yield 12;
+            } else {
+                yield 4;
+            }
+        }
+    }
 
-    //         for i in range(this.num_forms_in_this_section):
-    //             zero_index = next(uint_iterator) - 1
-    //             # print(
-    //             #     f"{FORMS[zero_index]=}, {shortcut_freq=}, {this.num_forms_in_this_section=}"
-    //             # )
-    //             yield FORMS[zero_index], shortcut_freq
-    //             this.num_forms_to_go -= 1
 
-    //     if this.num_forms_to_go == 0:
-    //         return
+    *formsAndFreqs(): IterableIterator<[string, number]> {
+        const uintIterator: IterableIterator<number> = intDecoder(this.rawBytes, null, this.bitWidths());
 
-    //     for uint in uint_iterator:
-    //         zero_index = uint - 1
-    //         # print(f"{uint=}, {zero_index=}, {FORMS[zero_index]=}")
-    //         yield FORMS[zero_index], next(uint_iterator)
-    //         this.num_forms_to_go -= 1
-    //         if this.num_forms_to_go == 0:
-    //             break
+        this.totalNumForms = this.numFormsToGo = next(uintIterator);
+        for (const uint of uintIterator) {
+            this.numFormsInThisSection = next(uintIterator);
+
+            if (uint === 0b000) {
+           
+                if (this.numFormsInThisSection !== this.numFormsToGo) {
+                    throw new Error(
+                        `Num forms in non-shortcutted section ${this.numFormsInThisSection}, `
+                        +`the last section, should equal the remaining number of forms to go ${this.numFormsToGo}`
+                        +'(after the number of forms in each previous sections has been subtracted off).'
+                    );
+                }
+                this.pastZeroMarker = true;
+                break;
+            }
+            const shortcut_freq = uint;
+
+            for (let i = 0; i < this.numFormsInThisSection; i++) {
+                
+                const zeroIndex = next(uintIterator) - 1;
+                yield [FORMS[zeroIndex], shortcut_freq];
+                this.numFormsToGo -= 1
+            }
+        }
+
+        if (this.numFormsToGo === 0) {
+            return;
+        }
+
+        for (const uint of uintIterator) {
+            const zeroIndex = uint - 1;
+            yield [FORMS[zeroIndex], next(uintIterator)];
+            this.numFormsToGo -= 1;
+            if (this.numFormsToGo === 0) {
+                break;
+            }
+        }
+
+
+    }
 }
