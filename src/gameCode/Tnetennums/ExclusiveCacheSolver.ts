748b4418319892642ad3efbc87f0c26131384242
JamesParrott <80779630+JamesParrott@users.noreply.github.com>
2025-01-10 12:28:16 +0000

    Refactor out SolutionInfo into its own file.

---
+++ src/gameCode/Tnetennums/ExclusiveCacheSolver.ts
@@ -0,0 +1,15 @@
+
+
+import {SolutionInfo} from './SolutionInfo';
+
+
+
+function* solveAndBuildExclusiveCache(
+    seeds: number[],
+    goal: number,
+    max_num_seeds: number | null = null,
+): IterableIterator<SolutionInfo> {
+
+
+
+}
