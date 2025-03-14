5f0451f8614decb0552e49a038cd661add3f1d5c
JamesParrott <80779630+JamesParrott@users.noreply.github.com>
2025-03-14 15:01:58 +0000

    Don't display subtitle if screen too narrow

--- src/components/NumbersGame/TitleBanner.tsx
+++ src/components/NumbersGame/TitleBanner.tsx
@@ -1,7 +1,12 @@
+import { useMediaQuery } from '@mantine/hooks';
+
 import {Group, Text, Title} from '@mantine/core';
 import classes from './TitleBanner.module.css';
 
 export function TitleBanner() {
+
+  const isMobile = useMediaQuery(`(max-width: 400px)`);
+
   return <Title className={classes.title} ta="center" mt={0}>
           <Group justify="center" align="end">
             <Text
@@ -12,9 +17,9 @@ export function TitleBanner() {
             >
               Numberz
             </Text>
-            <Text component="span" size="lg" mb={4.5}>
+            {isMobile || <Text component="span" size="lg" mb={4.5}>
               A numbers game.
-            </Text>
+            </Text>}
           </Group>
   </Title>
 }
